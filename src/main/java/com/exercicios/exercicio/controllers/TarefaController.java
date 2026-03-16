c4db04a48d15c7ea6a5bddd7ed415d2c5f7deb45
gmiashiro <g.miashiro.freitas@gmail.com>
2026-03-16 19:52:51 -0300

    feat: adiciona camada controllers

---
+++ src/main/java/com/exercicios/exercicio/controllers/TarefaController.java
@@ -0,0 +1,37 @@
+package com.exercicios.exercicio.controllers;
+
+import com.exercicios.exercicio.model.TarefaModel;
+import com.exercicios.exercicio.services.TarefaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping(path = "/tarefa")
+public class TarefaController {
+
+    @Autowired
+    TarefaService tarefaService;
+
+    @PostMapping
+    public TarefaModel salvar(@RequestBody TarefaModel tarefaModel) {
+        return tarefaService.save(tarefaModel);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletar(@PathVariable Long id) {
+        tarefaService.deleteById(id);
+    }
+
+    @GetMapping
+    public List<TarefaModel> listar() {
+        return tarefaService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public TarefaModel buscar(@PathVariable Long id) {
+        return tarefaService.findById(id);
+    }
+}
